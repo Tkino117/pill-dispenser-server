398a7d38ca2eddda4966da9f016678e9c6f6196b
Tkino117-remote <kinoshi117@gmail.com>
2024-10-08 16:28:35 +0900

    tcp success

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
 import java.net.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        int port = 12345;
+        int port = 8080;
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening on port " + port);
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    System.out.println("New client connected");
+                try (Socket clientSocket = serverSocket.accept()) {
+                    System.out.println("Client connected: " + clientSocket.getInetAddress());
 
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    String message = in.readLine();
+                    System.out.println("Received message: " + message);
+
+                    System.out.println("Closing connection");
                 }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Server exception: " + e.getMessage());
             e.printStackTrace();
         }
-
     }
 }
